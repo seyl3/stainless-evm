c40eeff25f935f3c99df367b7cee120f51ed9e7e
seyl3 <elyes.benabid@epfl.ch>
2026-06-29 03:40:11 +0200

    feat: add verified Stack with push/pop

---
+++ evm/src/main/scala/evm/Stack.scala
@@ -0,0 +1,27 @@
+package evm
+
+import stainless.collection.*
+import stainless.lang.*
+import evm.core.Word256
+
+object Stack {
+    val MAXIMUM_STACK_SIZE: BigInt = 1024
+}
+
+case class Stack(data: List[Word256]) {
+    require(data.size >= 0 && data.size <= Stack.MAXIMUM_STACK_SIZE)
+
+    def push(value: Word256): Stack = {
+        require(data.size < Stack.MAXIMUM_STACK_SIZE)
+
+        Stack(Cons(value, data))
+
+    }.ensuring(result => result.data.size == data.size + 1)
+
+    def pop(): (Word256, Stack) = {
+        require(data.nonEmpty)
+
+        (data.head, Stack(data.tail))
+
+    }.ensuring(result => result._2.data.size == data.size - 1)
+}
