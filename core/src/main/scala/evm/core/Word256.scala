117585abbea4c9a440f30d5572b694bc54cd9223
seyl3 <elyes.benabid@epfl.ch>
2026-06-30 02:34:14 +0200

    feat: add signed division and modulo to Word256

--- core/src/main/scala/evm/core/Word256.scala
+++ core/src/main/scala/evm/core/Word256.scala
@@ -4,14 +4,12 @@ import stainless.lang._
 import stainless.annotation._
 import stainless.proof._
 import evm.proofs.EvmMath
-import evm.proofs.EvmMath.{MODULO, MAX_VALUE, pow}
+import evm.proofs.EvmMath.{MODULO, MAX_VALUE, pow, inBounds}
 import evm.proofs.Bitwise
 
 object Word256 {
   val Zero: Word256 = Word256(BigInt(0))
   val One: Word256  = Word256(BigInt(1))
-
-  def inBounds(v: BigInt): Boolean = v >= 0 && v <= MAX_VALUE
 
   def addmod(a: Word256, b: Word256, n: Word256): Word256 = {
     if (n == Zero) Zero
@@ -31,25 +29,25 @@ object Word256 {
 }
 
 case class Word256(value: BigInt) {
-  require(Word256.inBounds(value))
+  require(inBounds(value))
 
   def +(other: Word256): Word256 = {
     Word256((value + other.value) % MODULO)
   }.ensuring(result =>
     result.value == (value + other.value) % MODULO
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def -(other: Word256): Word256 = {
     Word256(((value - other.value) + MODULO) % MODULO)
   }.ensuring(result =>
     result.value == (value - other.value + MODULO) % MODULO
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def *(other: Word256): Word256 = {
     Word256((value * other.value) % MODULO)
   }.ensuring(result =>
     result.value == (value * other.value) % MODULO
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def /(other: Word256): Word256 = {
     if (other == Word256.Zero) Word256.Zero
@@ -57,7 +55,7 @@ case class Word256(value: BigInt) {
   }.ensuring(result =>
     (if (other == Word256.Zero) result.value == BigInt(0)
      else result.value == value / other.value)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def %(other: Word256): Word256 = {
     if (other == Word256.Zero) Word256.Zero
@@ -65,41 +63,63 @@ case class Word256(value: BigInt) {
   }.ensuring(result =>
     (if (other == Word256.Zero) result.value == BigInt(0)
      else result.value == value % other.value)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
+
+  def sdiv(other: Word256): Word256 = {
+    if (other == Word256.Zero) Word256.Zero
+    else {
+      EvmMath.toSignedNonZero(other.value)
+      Word256(EvmMath.wrap(EvmMath.toSigned(value) / EvmMath.toSigned(other.value)))
+    }
+  }.ensuring(result =>
+    (if (other == Word256.Zero) result.value == BigInt(0)
+     else result.value == EvmMath.wrap(EvmMath.toSigned(value) / EvmMath.toSigned(other.value)))
+    && inBounds(result.value))
+
+  def smod(other: Word256): Word256 = {
+    if (other == Word256.Zero) Word256.Zero
+    else {
+      EvmMath.toSignedNonZero(other.value)
+      Word256(EvmMath.wrap(EvmMath.toSigned(value) % EvmMath.toSigned(other.value)))
+    }
+  }.ensuring(result =>
+    (if (other == Word256.Zero) result.value == BigInt(0)
+     else result.value == EvmMath.wrap(EvmMath.toSigned(value) % EvmMath.toSigned(other.value)))
+    && inBounds(result.value))
 
   def &(other: Word256): Word256 = {
     Bitwise.andBound(value, other.value)
     Word256(Bitwise.and(value, other.value))
   }.ensuring(result =>
     result.value == Bitwise.and(value, other.value)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def |(other: Word256): Word256 = {
     Bitwise.orBound(value, other.value)
     Word256(Bitwise.or(value, other.value))
   }.ensuring(result =>
     result.value == Bitwise.or(value, other.value)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def ^(other: Word256): Word256 = {
     Bitwise.xorBound(value, other.value)
     Word256(Bitwise.xor(value, other.value))
   }.ensuring(result =>
     result.value == Bitwise.xor(value, other.value)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def unary_~ : Word256 = {
     Word256(Bitwise.not(value))
   }.ensuring(result =>
     result.value == MAX_VALUE - value
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def **(exp: Word256): Word256 = {
     EvmMath.powNonNeg(value, exp.value)
     Word256(pow(value, exp.value) % MODULO)
   }.ensuring(result =>
     result.value == pow(value, exp.value) % MODULO
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def <<(shift: Word256): Word256 = {
     EvmMath.powNonNeg(BigInt(2), shift.value)
@@ -108,7 +128,7 @@ case class Word256(value: BigInt) {
   }.ensuring(result =>
     (if (shift.value >= BigInt(256)) result.value == BigInt(0)
      else result.value == (value * pow(BigInt(2), shift.value)) % MODULO)
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 
   def >>(shift: Word256): Word256 = {
     EvmMath.powTwoPos(shift.value)
@@ -117,5 +137,5 @@ case class Word256(value: BigInt) {
   }.ensuring(result =>
     (if (shift.value >= BigInt(256)) result.value == BigInt(0)
      else result.value == value / pow(BigInt(2), shift.value))
-    && Word256.inBounds(result.value))
+    && inBounds(result.value))
 }
