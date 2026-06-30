5cc88e1cc409beba35f58d6e8c5daa35a260c2e0
seyl3 <elyes.benabid@epfl.ch>
2026-06-30 15:55:07 +0200

    test: add Stack peek, dup and swap unit tests

--- evm/src/test/scala/evm/StackSuite.scala
+++ evm/src/test/scala/evm/StackSuite.scala
@@ -34,4 +34,36 @@ class StackSuite extends munit.FunSuite {
     assertEquals(top.value, BigInt(9))
     assertEquals(rest.data.size, s0.data.size)
   }
+
+  test("peek reads by index from the top without popping") {
+    val s = build(10, 20, 30)
+    assertEquals(s.peek(0).value, BigInt(10))
+    assertEquals(s.peek(1).value, BigInt(20))
+    assertEquals(s.peek(2).value, BigInt(30))
+    assertEquals(s.data.size, BigInt(3))
+  }
+
+  test("dup(n) clones the n-th item onto the top") {
+    val s = build(10, 20, 30)
+    val d1 = s.dup(1)
+    assertEquals(d1.data.size, BigInt(4))
+    assertEquals(d1.peek(0).value, BigInt(10))
+    assertEquals(d1.peek(1).value, BigInt(10))
+    val d3 = s.dup(3)
+    assertEquals(d3.peek(0).value, BigInt(30))
+    assertEquals(d3.peek(1).value, BigInt(10))
+  }
+
+  test("swap(n) swaps the top with the (n+1)-th item") {
+    val s = build(10, 20, 30, 40)
+    val s1 = s.swap(1)
+    assertEquals(s1.peek(0).value, BigInt(20))
+    assertEquals(s1.peek(1).value, BigInt(10))
+    assertEquals(s1.peek(2).value, BigInt(30))
+    val s3 = s.swap(3)
+    assertEquals(s3.peek(0).value, BigInt(40))
+    assertEquals(s3.peek(3).value, BigInt(10))
+    assertEquals(s3.peek(1).value, BigInt(20))
+    assertEquals(s3.data.size, BigInt(4))
+  }
 }
