13ad7729296a1cab5ff7725cf9096c0804dc2c2e
seyl3 <elyes.benabid@epfl.ch>
2026-07-01 02:09:31 +0200

    feat: add JUMPDEST analysis for valid jump targets

--- evm/src/main/scala/evm/Code.scala
+++ evm/src/main/scala/evm/Code.scala
@@ -19,3 +19,21 @@ case class Code(code: List[Int]):
     require(0 <= pc && pc < code.size)
     Opcode.decode(code(pc))
   }
+
+  def jumpDestsFrom(i: BigInt, acc: Set[BigInt]): Set[BigInt] = {
+    require(0 <= i && i <= code.size)
+    decreases(code.size - i)
+    if (i >= code.size) acc
+    else {
+      val w = Opcode.decode(code(i)) match
+        case Some(op) => Opcode.pushWidth(op)
+        case None() => BigInt(0)
+      val acc2 = if (Opcode.decode(code(i)) == Some(Opcode.JUMPDEST)) acc ++ Set(i) else acc
+      val next = i + 1 + w
+      jumpDestsFrom(if (next > code.size) code.size else next, acc2)
+    }
+  }
+
+  def validJumpDests: Set[BigInt] = jumpDestsFrom(0, Set.empty[BigInt])
+
+  def isValidJumpDest(pc: BigInt): Boolean = validJumpDests.contains(pc)
