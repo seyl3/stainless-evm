9254584074d847e2029f011f3dcfa52f33f4c439
seyl3 <elyes.benabid@epfl.ch>
2026-07-02 15:44:08 +0100

    feat: add Address, block/tx/message context and world state

---
+++ evm/src/main/scala/evm/env/Address.scala
@@ -0,0 +1,10 @@
+package evm.env
+
+import evm.proofs.EvmMath.pow
+
+object Address:
+  val MODULO: BigInt = pow(BigInt(2), BigInt(160))
+  def zero: Address = Address(BigInt(0))
+
+case class Address(value: BigInt):
+  require(0 <= value && value < Address.MODULO)
